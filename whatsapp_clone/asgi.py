f24259ae71f5ec830ba4e3163a2a2819322973e0
Ankur586 <ankur.singh586@gmail.com>
2023-10-18 23:26:38 +0530

    chat_app_underprocess

--- whatsapp_clone/asgi.py
+++ whatsapp_clone/asgi.py
@@ -8,12 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter # this added
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter,URLRouter # this added
+from send_message.routing import websocket_urlpatterns
+import send_message.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsapp_clone.settings')
-
+django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
 })
