5dec73119c32cc600895be4f1bb4f6f6e34e6bea
Bharatsingh.thakur@leadsconnect.in <bharatsingh.thakur@leadsconnect.in>
2023-10-19 18:05:44 +0530

    underprocess

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # whatsapp_clone
+
+# pip install -r requirements.txt --use-pep517
+
+# python manage.py migrate
+
+# python manage.py runserver
--- send_message/views.py
+++ send_message/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def test(request):
+def index(request):
     return render(request,'send_message/index.html')
 
 def room(request, room_name):
