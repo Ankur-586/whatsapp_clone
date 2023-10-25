f9042438ad23abe30afaaccb84e73f359ab524c2
Ankur586 <ankur.singh586@gmail.com>
2023-10-25 23:14:21 +0530

    practice

--- str.py
+++ str.py
@@ -20,18 +20,28 @@
 # s1 = s.replace('4','9')
 # print(s1)
 
-item_cart = []
-class item():
-    def __init__(self,name: str, price: int):
-        self.item_name = name
-        self.item_price = price
-    def show_item(self):
-        self.n
-class ShoppingCart():
-    def add(self):
-        Name = input("Enter Item Name: ")
-        Price = int(input("Enter Price: "))
-        item1 = item(Name,Price)
-        print(item1)
-s1 = ShoppingCart()
-s1.add()
+# item_cart = []
+# class item:
+#     def __init__(self,name: str, price: int):
+#         self.item_name = name
+#         self.item_price = price
+#     def show_item(self):
+#         return self.item_name, self.item_price
+    
+# class ShoppingCart:
+#     def add(self):
+#         Name = input("Enter Item Name: ")
+#         Price = int(input("Enter Price: "))
+#         item1 = item(Name,Price)
+#         item_cart.append(item1.show_item())
+#         # print(item_cart)
+
+# s1 = ShoppingCart()
+# s1.add()
+
+'''
+The lowest grade of  belongs to Tina. The second lowest grade of  belongs to both Harry and Berry, 
+so we order their names alphabetically and print each name on a new line.
+'''
+students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
